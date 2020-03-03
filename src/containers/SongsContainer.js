f3d3fc0518e243414a66e83313c8492662d48067
Cody Abbott <cody.abbott16@gmail.com>
2020-03-03 15:43:20 +0000

    fetches from api and creates array of song objects

---
+++ src/containers/SongsContainer.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+export default class SongsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      songs: [],
+      selectedSongId: ""
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+
+    fetch(url)
+      .then(response => response.json())
+      .then(result => result.feed)
+      .then(entries => entries.entry)
+      .then(songs => this.setState({ songs: songs }))
+      .catch(error => console.error);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Top 20 Songs on Itunes</h2>
+      </div>
+    );
+  }
+}
